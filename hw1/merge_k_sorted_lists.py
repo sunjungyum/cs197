37dc7ce91f69dd8eeea29ac079096f327f544e61
Sun-Jung Yum <sunjungyum@college.harvard.edu>
2022-09-12 11:34:36 -0400

    Create .py file and environment.yml
    This commit includes the base Python file for the merge_k_sorted_lists
    problem, including the necessary class definitions. It also exports the
    environment in a .yml file.

---
+++ hw1/merge_k_sorted_lists.py
@@ -0,0 +1,16 @@
+import heapq
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        return
+        
